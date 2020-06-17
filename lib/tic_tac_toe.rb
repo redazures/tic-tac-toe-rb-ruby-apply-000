6fe57b058aa8e51d2f67332634c68e8c3fbd361c
james wu <jwu_nyc@yahoo.com>
2020-06-17 00:49:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def turn(board)
     input=gets.strip
     index=input_to_index(input)
   end
-  move(board,index,"X")
+  move(board,index,current_player(board))
   display_board(board)
   #puts 1+converted_number
 end
